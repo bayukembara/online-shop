7c1979fc31ee86522fc3178ad0a44b00df083e69
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-02-03 00:56:22 +0700

    review-product database

--- migrations/20230202174816-create-review.js
+++ migrations/20230202174816-create-review.js
@@ -1,43 +1,43 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      productid: {
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      stars: {
-        type: Sequelize.INTEGER
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-      reviews: {
-        type: Sequelize.STRING
-      },
-      date: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Reviews", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            productid: {
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            stars: {
+                type: Sequelize.INTEGER,
+            },
+            image: {
+                type: Sequelize.STRING,
+            },
+            reviews: {
+                type: Sequelize.STRING,
+            },
+            date: {
+                type: Sequelize.DATE,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Reviews");
+    },
 };
--- models/review.js
+++ models/review.js
@@ -1,28 +1,30 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Review extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Review extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            Review.belongsTo(models.Product);
+            models.Product.hasMany(Review);
+        }
     }
-  }
-  Review.init({
-    productid: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    stars: DataTypes.INTEGER,
-    image: DataTypes.STRING,
-    reviews: DataTypes.STRING,
-    date: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Review',
-  });
-  return Review;
+    Review.init(
+        {
+            productid: DataTypes.INTEGER,
+            name: DataTypes.STRING,
+            stars: DataTypes.INTEGER,
+            image: DataTypes.STRING,
+            reviews: DataTypes.STRING,
+            date: DataTypes.DATE,
+        },
+        {
+            sequelize,
+            modelName: "Review",
+        }
+    );
+    return Review;
 };
